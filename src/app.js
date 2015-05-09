6adde3979317e8406e0426c5be95b21a60130144
Chad Karon <ctk5520@g.rit.edu>
2015-05-09 11:40:49 -0400

    -Fixed files that sourcetree broke

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var compression = require('compression');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
-<<<<<<< HEAD
 var router = require('./router.js');
 var http = require("http");
 var socketio = require('socket.io')
@@ -23,25 +22,6 @@ app.use(compression());
 app.use(favicon(__dirname + '/client/img/favicon.png'));
 app.use(cookieParser());
 
-=======
-var socketio = require('socket.io');
-var router = require('./router.js');
-
-var app;     
-var server;  
-var port = process.env.PORT || process.env.NODE_PORT || 3000;
-
-var app = express();
-app.use('/assets', express.static(path.resolve(__dirname+'../../client/')));
-app.use(compression());
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
-app.use(favicon(__dirname + '/../client/img/favicon.png'));
-app.use(cookieParser());
-
-router(app);
-
->>>>>>> 51fddcbeefcaf4866452210e81459dc49855bedd
 server = app.listen(port, function(err) {
     if (err) {
       throw err;
@@ -49,7 +29,6 @@ server = app.listen(port, function(err) {
     console.log('Listening on port ' + port);
 });
 
-<<<<<<< HEAD
 var io = socketio.listen(server);
 
 io.sockets.on("connect", function(socket){
@@ -60,5 +39,3 @@ io.sockets.on("connect", function(socket){
         socket.join("room1");
 	});
 });
-=======
->>>>>>> 51fddcbeefcaf4866452210e81459dc49855bedd
